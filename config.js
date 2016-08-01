c1b589c36de597784c29d7e615299f8d37d2adc3
haihoang202 <haihoang202@yahoo.com>
2016-07-31 19:03:09 -0500

    'Hoang'

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  token: "xoxb-64735170163-3zOGiDCYNhsqPiJtzu1Wq0MA"
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+var bot = controller.spawn({
+  token: require('./config').token
+})
+bot.startRTM(function(err,bot,payload) {
+  if (err) {
+    console.log(err);
+    throw new Error('Could not connect to Slack');
+  }
+});
+
+controller.hears(["hey","^pattern$"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+  // do something to respond to message
+  // all of the fields available in a normal Slack message object are available
+  // https://api.slack.com/events/message
+  bot.reply(message, {
+    text:"A more complex response",
+    username: "ReplyBot",
+    icon_emoji: ":ghost:"
+  });
+});
+
+controller.on('ambient', function(bot,message){
+  bot.reply(message, {
+    text:"A more complex response",
+    username: "ReplyBot",
+    icon_emoji: ":ghost:"
+  })
+})
