f2ca29c98eebd366144b33977b3e0c6096fb57d1
haihoang202 <haihoang202@yahoo.com>
2016-07-31 19:47:23 -0500

    'hoang'

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.listen(port, function(){
 app.post('/hello', function(req, res, next){
   var userName = req.body.user_name;
   var botPayload = {
-    text:'Hello ' + userName + '. Welcome to Hoang Dev Team'
+    // text:'Hello ' + userName + '. Welcome to Hoang Dev Team'
+    text: req
   };
 
   if(userName !== 'slackbot'){
