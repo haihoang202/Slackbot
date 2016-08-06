16ad035c92a6fe3c4bb58e2392ba0ff45a4e2a22
haihoang202 <haihoang202@yahoo.com>
2016-08-06 16:02:29 -0500

    hoang

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
   // res.status(200).send('Hello World!');
-  var re;
+  var re =
   unirest.get("https://mashape-community-urban-dictionary.p.mashape.com/define?term=watch")
         .header("X-Mashape-Key", "2f5jJRAZVsmshu3LtG1ho3JoOEL9p1cKCrfjsna4vtPBumLj5p")
         .header("Accept", "text/plain")
