63dd2fdb93c6e0b9e49ccccb745bf498c9529254
haihoang202 <haihoang202@yahoo.com>
2016-08-31 02:21:00 -0500

    hoang

--- app.js
+++ app.js
@@ -67,6 +67,9 @@ app.post('/def', function(req, res, next){
   word1 = urban(word);
   word1.first(function(json){
     re = word + " means: " + json.definition;
+    // if(typeof json.definition == 'undefined'){
+    //
+    // }
     // console.log("Divide 1: \n");
     // console.log(re);
     // console.log("Divide 2: \n");
@@ -75,6 +78,12 @@ app.post('/def', function(req, res, next){
 
     payload["response_type"] = response_type;
     payload["text"] = re;
+    payload["error"]={};
+    payload["error"]["code"]=2;
+    payload["error"]["msg"]="Temporarily ungathered :/";
+
+    console.log(payload);
+
     if(userName !== 'slackbot'){
       return res.status(200).json(payload);
     }
